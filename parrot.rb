d1cbb31808fb0a54f310e2deceba38dd9498f5d0
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-26 14:16:20 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot (sound = "Squawk!", phrase = "Pretty bird!")
-@sound = "Squawk!"
 @sound = sound
 return "#{@sound}"
 end
